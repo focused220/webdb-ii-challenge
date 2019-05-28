10bd9a55efc5cd86a6537f3a0a6e622399ceb000
focused220 <roblhjr.220@gmail.com>
2019-05-28 15:12:18 -0700

    initial commit and setup complete

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const knex = require('knex');
+const knexConfig = require('./knexfile.js');
+const db = knex(knexConfig.development);
 const helmet = require('helmet');
 
 const server = express();
@@ -7,6 +10,47 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+
+server.post('/api/zoos', async (req, res) => {
+  console.log(req.body)
+  try{
+    const newName = req.body;
+    const newZoo = await db.insert(newName).into('zoos').then(zoo => {res.status(201).json(zoo)})
+  }catch(error){res.status(500).json(`{error: Unable to add new Zoom item}`)}
+
+})
+
+server.get('/api/zoos', async (req, res) => {
+  try{
+    const allZoos = await db('zoos').then(zoo =>{res.status(201).json(zoo)})
+  }
+  catch(error){ res.status(500).json(`{error: unable to retrieve zoos}`)}
+})
+
+server.get('/api/zoos/:id', async (req, res) => {
+  const {id} = req.params
+  try{
+    const allZoos = await db('zoos').where('id', '=', id).then(zoo =>{res.status(201).json(zoo)})
+  }
+  catch(error){ res.status(500).json(`{error: unable to retrieve zoos}`)}
+})
+
+server.put('/api/zoos/:id', async (req, res) => {
+  const {id} = req.params
+  const changes = req.body
+  try{
+    const updateZoo = await db('zoos').where('id', '=', id).update(changes).then(item => {res.status(201).json(item)})
+  }catch(error){res.status(500).json(`{error: unable to update zoo}`)}
+})
+
+server.delete('/api/zoos/:id', async (req, res) => {
+  const {id} = req.params
+
+  try{
+    const zoo = await db('zoos').where('id', '=', id).del().then(item => {res.status(201).json(item)})
+  }
+  catch(error){ res.status(500).json(`{error: unable to retrieve zoos}`)}
+})
 
 const port = 3300;
 server.listen(port, function() {
